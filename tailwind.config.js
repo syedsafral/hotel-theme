ad5760e81b742b442f2285d2e2a44563ed79833c
syed safral <saiyedsafral@gmail.com>
2022-05-27 20:24:12 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/src/01.jpg')",
+        
+      }
+      
+    },
+  },
+  plugins: [],
+}
